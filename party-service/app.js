49732e58afca4fa248a670001196c5a9f8e32414
pmajum <prasenjit_maj06@yahoo.co.in>
2020-03-05 16:02:38 +0530

    Updated the helm chart to show multiple deployments, upgrade & rollback

---
+++ party-service/app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const os = require('os');
+console.log("Kubia server starting...");
+
+var handler = function(request, response) {
+ console.log("Received request from " + request.connection.remoteAddress);
+ response.writeHead(200);
+ response.end("This is party service running version V2 in pod " + os.hostname() + "\n");
+};
+var www = http.createServer(handler);
+www.listen(8080);
